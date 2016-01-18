4ea8baf6a6ca4ad45c4caabb619f3e1cbabcae27
ajohn2550 <alex@johnsonwebware.com>
2016-01-18 14:50:11 -0700

    Starting routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-global.lib = __dirname + '/lib';
-global.root = __dirname;
+global.lib    = __dirname + '/lib';
+global.root   = __dirname;
 global.config = require('./config')[process.env.NODE_ENV || 'dev'];
-global.util = require('util');
+global.util   = require('util');
 
-var app = require(global.lib + '/init');
+require(global.lib + '/init')
+	.set('view engine', 'jade')
+;
